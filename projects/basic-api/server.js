09d65834bc44f60ee5daab1243635ede3e478027
Miguel <laigmiguel@gmail.com>
2026-06-01 10:52:53 -0300

    Add get user by id route

--- projects/basic-api/server.js
+++ projects/basic-api/server.js
@@ -17,6 +17,18 @@ app.get('/users', (req, res) => {
   res.json(users)
 })
 
+app.get('/users/:id', (req, res) => {
+  const reqId = Number(req.params.id)
+
+  const user = users.find((user) => user.id === reqId)
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  res.json(user)
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
 })
