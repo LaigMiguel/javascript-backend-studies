d4d77390b110a7db343d64317287e0fa8ecd7fa1
Miguel <laigmiguel@gmail.com>
2026-07-14 12:24:06 -0300

    chore: log unexpected server errors

--- projects/basic-api/middlewares/errorMiddleware.js
+++ projects/basic-api/middlewares/errorMiddleware.js
@@ -3,9 +3,9 @@ const AppError = require('../errors/appError')
 function errorMiddleware(error, req, res, next) {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message })
-  } else {
-    return res.status(500).json({ message: 'Internal Server Error' })
   }
+  console.error(error)
+  return res.status(500).json({ message: 'Internal Server Error' })
 }
 
 module.exports = errorMiddleware
